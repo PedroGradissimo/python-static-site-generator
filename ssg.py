ff84ff1051fa543c63aa11beb53df715b1112b11
PedroGradissimo <gradispe@gmail.com>
2021-08-05 23:21:40 +0100

    Changed the value of source from "Content" to "content"

--- ssg.py
+++ ssg.py
@@ -1,10 +1,9 @@
 # Typer is a built-in library for building CLI applications
 import typer
-
 from ssg.site import Site
 
 
-def main(source="Content", dest="dist"):
+def main(source="content", dest="dist"):
     config = {"source": source, "dest": dest}
     Site(**config).build()
 
